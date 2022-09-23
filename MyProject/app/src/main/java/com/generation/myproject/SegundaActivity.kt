156e064ae3eb87caf17a1d33707bd5083c97dfe9
Gabriela <gabrielagheleno@gmail.com>
2022-09-23 18:36:28 -0300

    Entrega dos projetos Activities/Intents e Navigation Component

---
+++ MyProject/app/src/main/java/com/generation/myproject/SegundaActivity.kt
@@ -0,0 +1,20 @@
+package com.generation.myproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SegundaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segunda)
+
+        val buttonBack = findViewById<Button>(R.id.buttonBack)
+
+        buttonBack.setOnClickListener {
+            finish()
+        }
+
+    }
+
+}
